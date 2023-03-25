db37e2a4ee90c21a7393570348b59c034923771d
Chris Germany <chris.germany@blueyonder.co.uk>
2023-03-25 16:40:51 +0000

    uodate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import { fetchWithTimout } from './services';
+
 const movies = require('./data/movies.json');
 
 export function fetchMovies(){
   const resolveFunction = () => movies;
+
+  return fetchWithTimeout(1000).then(resolveFunction);
 }
+
+const moviePromise = fetchMovies()
