197a7f96fcbedc4cb4cd9aafb3b3494680a389f3
Chris Germany <chris.germany@blueyonder.co.uk>
2023-03-25 17:04:42 +0000

    updates

--- src/services.js
+++ src/services.js
@@ -1,3 +1,16 @@
 export function fetchWithTimeout(delay) {
   return new Promise(resolve => setTimeout(resolve, delay));
 };
+
+export function fetchMovies(){
+  return fetch("./data/movies.json")
+    .then(response => response.json())
+    .then(movie => movies)
+    .catch(error => console.log(error));
+    
+  const resolveFunction = () => movies;
+
+  return fetchWithTimeout(1000).then(resolveFunction);
+}
+
+const moviePromise = fetchMovies();
