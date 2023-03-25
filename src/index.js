7452d33314eb545da0c4facb8ea521b6807e00c1
Chris Germany <chris.germany@blueyonder.co.uk>
2023-03-25 17:36:51 +0000

    updates

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import { fetchWithTimeout, ferchMovies, fetchBooks } from './services';
 const movies = require('./data/movies.json');
+
+const getBooksAndMovies = () => {
+  return Promise.all([fetchBooks(), fetchMoview()])
+    .then(([books, movies]) => ({
+      books,
+      movies,
+    }))
+    .catch(error = console.log('Error fetching books and movies', error))
+};
+
